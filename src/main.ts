f04559246e8ed94e7977443a69cc46f066341f74
wenderf5 <wenderfabianoalves@gmail.com>
2024-09-23 18:56:09 -0300

    fix: sign-in

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://sistema-login-front-end.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
